76d655a80f1ac4286f22c4d340642954392d6be3
harissarwar1 <muhammad.haris400008@gmail.com>
2023-11-26 23:21:07 +0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList></ProductList>
+    </div>
+  );
+}
+
+export default App;
